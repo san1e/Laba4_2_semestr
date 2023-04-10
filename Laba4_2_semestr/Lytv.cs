a99e99d65304db6074e51d18e3615cd0e2ba393b
Олександр Клименко <alklimenko227@outlook.com>
2023-04-10 19:57:46 +0300

    Добавьте файлы проекта.

---
+++ Laba4_2_semestr/Lytv.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba4_2_semestr
+{
+    public partial class Lytv
+    {
+        static void Main()
+        {
+
+        }
+    }
+}
---
+++ Laba4_2_semestr/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Laba4_2_semestr
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Klym klym= new Klym();
+            klym.Main();
+        }
+    }
+}
