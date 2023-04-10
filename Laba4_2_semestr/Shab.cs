b00df5975def5245c2aa569aa24c073bf4f6946d
Олександр Клименко <alklimenko227@outlook.com>
2023-04-10 23:07:58 +0300

    Klym(1 task,2 task)

--- Laba4_2_semestr/Shab.cs
+++ Laba4_2_semestr/Shab.cs
@@ -8,9 +8,41 @@ namespace Laba4_2_semestr
 {
     public partial class Shab
     {
-        static void Main()
+        static void Task1()
         {
 
+        }
+        static void Task2()
+        {
+
+        }
+        static void Task3()
+        {
+
+        }
+        public void Main()
+        {
+            int choice;
+            do
+            {
+                Console.WriteLine("Задача 1 - 1");
+                Console.WriteLine("Задача 2 - 2");
+                Console.WriteLine("Задача 3 - 3");
+                choice = int.Parse(Console.ReadLine());
+                Console.Clear();
+                switch (choice)
+                {
+                    case 1:
+                        Task1();
+                        break;
+                    case 2:
+                        Task2();
+                        break;
+                    case 3:
+                        Task3();
+                        break;
+                }
+            } while (choice != 0);
         }
     }
 }
